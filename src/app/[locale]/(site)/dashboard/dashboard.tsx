63dbda0dc7dcb413512341a8e47126ddc9e01550
Japangor <japan.gor@logicauto.io>
2024-03-22 12:26:49 -0400

    Update dashboard.tsx

--- src/app/[locale]/(site)/dashboard/dashboard.tsx
+++ src/app/[locale]/(site)/dashboard/dashboard.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import { Sidebar } from '@/components/components/Sidebar'
-import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,14 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers>
           <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
-            <SideBar />
+            <Sidebar />
             <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
               {children}
             </main>
           </div>
-        </Providers>
       </body>
     </html>
   )
